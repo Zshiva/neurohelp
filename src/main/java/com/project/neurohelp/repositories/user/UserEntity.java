5d524229f2eedb22195b5aae26069a36569ee540
unknown <zshiva35@gmail.com>
2025-06-09 17:40:29 +0545

    Add UserEntity class to represent users

---
+++ src/main/java/com/project/neurohelp/repositories/user/UserEntity.java
@@ -0,0 +1,9 @@
+package com.project.neurohelp.repositories.user;
+
+public class UserEntity {
+
+    private String id;
+    private String username;
+    private String email;
+    private Roles roles;
+}
