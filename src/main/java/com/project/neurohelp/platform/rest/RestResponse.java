174dc8c1430fbbca651854c89f23ac8dcdfa8c8c
Zshiva <zshiva35@gmail.com>
2026-05-02 16:36:58 +0545

    feat : add custom response for error and success with code and message

--- src/main/java/com/project/neurohelp/platform/rest/RestResponse.java
+++ src/main/java/com/project/neurohelp/platform/rest/RestResponse.java
@@ -1,4 +1,34 @@
 package com.project.neurohelp.platform.rest;
 
+import io.soabase.recordbuilder.core.RecordBuilder;
+
+@RecordBuilder
 public record RestResponse<T>(String code, String message, T data) {
+        public static <T> RestResponse success(){
+            return RestResponseBuilder.builder()
+                    .code("0")
+                    .message("SUCCESS")
+                    .data(null)
+                    .build();
+        }
+    public static <T> RestResponse success(T data) {
+        return RestResponseBuilder.builder()
+                .code("0")
+                .message("SUCCESS")
+                .data(data)
+                .build();
+    }
+    public static RestResponse error(String code, String message) {
+        return RestResponseBuilder.builder()
+                .code(code)
+                .message(message)
+                .build();
+    }
+    public static RestResponse error(String message) {
+        return RestResponseBuilder.builder()
+                .code("400")
+                .message(message)
+                .build();
+    }
+
 }
