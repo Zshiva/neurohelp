bf915e64bd7dd2d58a3e4bd933474475dd0180ab
Zshiva <zshiva35@gmail.com>
2026-05-02 16:35:26 +0545

    feat : add method call 'execute' with interface

--- src/main/java/com/project/neurohelp/platform/usecase/UseCase.java
+++ src/main/java/com/project/neurohelp/platform/usecase/UseCase.java
@@ -1,6 +1,12 @@
 package com.project.neurohelp.platform.usecase;
 
+import com.project.neurohelp.usecases.login.LoginUseCaseRequest;
+import com.project.neurohelp.usecases.login.LoginUseCaseResponse;
+
+import java.io.IOException;
 import java.util.Optional;
 
+@FunctionalInterface
 public interface UseCase <I extends UseCaseRequest, O extends UseCaseResponse>{
+    Optional<O> execute(I request) throws IOException, InterruptedException;
 }
