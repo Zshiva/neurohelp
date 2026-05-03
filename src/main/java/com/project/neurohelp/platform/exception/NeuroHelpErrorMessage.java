c6b00fca437b978a2d98a23de4d91a4a118ea5a6
Zshiva <zshiva35@gmail.com>
2026-05-03 10:14:44 +0545

    feat : add Error message for credentials

--- src/main/java/com/project/neurohelp/platform/exception/NeuroHelpErrorMessage.java
+++ src/main/java/com/project/neurohelp/platform/exception/NeuroHelpErrorMessage.java
@@ -1,10 +1,14 @@
 package com.project.neurohelp.platform.exception;
 
 public enum NeuroHelpErrorMessage {
-    LOGIN_UNSUCCESSFUL("NH001","User Login Unsuccessful"),
-    LOGIN_CREDENTIALS_DOES_NOT_MATCH("NHOO2","Login Credentials doesn't match")
-
-    ;
+    LOGIN_UNSUCCESSFUL("NH003","User Login Unsuccessful"),
+    LOGIN_CREDENTIALS_DOES_NOT_MATCH("NHOO4","Login Credentials doesn't match"),
+    REGISTER_UNSUCCESSFUL("NH001", "Register Unsuccessful" ),
+    EMAIL_ID_ALREADY_EXISTS("","Email ID Already Existed" ),
+    EMAIL_ID_REQUIRED("NH005", "Email ID is required" ),
+    CITIZENSHIP_RECORD_IS_REQUIRED("NH006","Citizenship is required" ),
+    CITIZENSHIP_RECORD_ALREADY_EXISTS("NH007","Citizenship Number already exists" ),
+    PASSWORD_IS_REQUIRED("NH006","Password is required" );
     private String code;
     private String message;
     NeuroHelpErrorMessage(String code, String message) {
