98bc1e6d61470601826a226ae6bb1578e5e75d92
Zshiva <zshiva35@gmail.com>
2026-05-07 10:44:51 +0545

    feat : add login request payload

--- src/main/java/com/project/neurohelp/controllers/payload/login/LoginUserRequestPayload.java
+++ src/main/java/com/project/neurohelp/controllers/payload/login/LoginUserRequestPayload.java
@@ -1,9 +1,9 @@
-package com.project.neurohelp.controllers.payload;
+package com.project.neurohelp.controllers.payload.login;
 
 import io.soabase.recordbuilder.core.RecordBuilder;
 
 @RecordBuilder
 public record LoginUserRequestPayload(
-        String emailId,
+        String email,
         String password
 ) { }
