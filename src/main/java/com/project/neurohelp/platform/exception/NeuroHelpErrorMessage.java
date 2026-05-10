70164202c64956a0975169b67fd46a7c6bec3d1b
Zshiva <zshiva35@gmail.com>
2026-05-10 12:33:55 +0545

    feat : add error message for otp invalid and same password

--- src/main/java/com/project/neurohelp/platform/exception/NeuroHelpErrorMessage.java
+++ src/main/java/com/project/neurohelp/platform/exception/NeuroHelpErrorMessage.java
@@ -8,7 +8,13 @@ public enum NeuroHelpErrorMessage {
     EMAIL_ID_REQUIRED("NH005", "Email ID is required" ),
     CITIZENSHIP_RECORD_IS_REQUIRED("NH006","Citizenship is required" ),
     CITIZENSHIP_RECORD_ALREADY_EXISTS("NH007","Citizenship Number already exists" ),
-    PASSWORD_IS_REQUIRED("NH006","Password is required" );
+    PASSWORD_IS_REQUIRED("NH006","Password is required" ),
+
+    USER_NOT_FOUND("NH008", "User not found"),
+    OTP_IS_REQUIRED("NH009", "OTP is required"),
+    OTP_INVALID_OR_EXPIRED("NH010", "OTP is invalid or expired"),
+    NEW_PASSWORD_REQUIRED("NH011", "New password is required"),
+    NEW_PASSWORD_MUST_BE_DIFFERENT("NH012", "New password must be different from old password");
     private String code;
     private String message;
     NeuroHelpErrorMessage(String code, String message) {
