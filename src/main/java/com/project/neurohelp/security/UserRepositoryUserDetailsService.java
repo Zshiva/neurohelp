ba9ab0dc2e529a4b2586572b6524d6fc8a5adc35
Zshiva <zshiva35@gmail.com>
2026-06-10 21:00:40 +0545

    feat : add userdetails service for jwt authentication

---
+++ src/main/java/com/project/neurohelp/security/UserRepositoryUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.project.neurohelp.security;
+
+import com.project.neurohelp.repositories.user.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserRepositoryUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserRepositoryUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        return userRepository.findByEmail(username)
+                .map(UserPrincipal::from)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+    }
+}
+
