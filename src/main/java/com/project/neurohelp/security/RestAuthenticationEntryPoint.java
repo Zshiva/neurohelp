d8c0ce22c61f165d8025311d9f11ff71b868bb5f
Zshiva <zshiva35@gmail.com>
2026-06-12 16:30:14 +0545

    feat : add httpservlet request, response and exception

---
+++ src/main/java/com/project/neurohelp/security/RestAuthenticationEntryPoint.java
@@ -0,0 +1,30 @@
+package com.project.neurohelp.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.neurohelp.platform.rest.RestResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final ObjectMapper objectMapper;
+
+    public RestAuthenticationEntryPoint(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+            throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        objectMapper.writeValue(response.getOutputStream(), RestResponse.error("401", "Unauthorized"));
+    }
+}
+
