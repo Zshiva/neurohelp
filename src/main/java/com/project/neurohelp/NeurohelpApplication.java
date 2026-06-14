8d10d0475dbe3637393d576aa52863204cd17bb4
Zshiva <zshiva35@gmail.com>
2026-06-14 20:50:11 +0545

    feat : add main springbootapplication

--- src/main/java/com/project/neurohelp/NeurohelpApplication.java
+++ src/main/java/com/project/neurohelp/NeurohelpApplication.java
@@ -2,8 +2,10 @@ package com.project.neurohelp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class NeurohelpApplication {
 
 	public static void main(String[] args) {
